a4142522fd81deaef8b0f796c654f46b38315f70
BUGyyc <846353427@qq.com>
2019-03-19 10:51:28 +0800

    两数相加

---
+++ 2.两数相加.java
@@ -0,0 +1,63 @@
+/*
+ * @lc app=leetcode.cn id=2 lang=java
+ *
+ * [2] 两数相加
+ *
+ * https://leetcode-cn.com/problems/add-two-numbers/description/
+ *
+ * algorithms
+ * Medium (33.03%)
+ * Total Accepted:    98.6K
+ * Total Submissions: 296.2K
+ * Testcase Example:  '[2,4,3]\n[5,6,4]'
+ *
+ * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+ * 
+ * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+ * 
+ * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ * 
+ * 示例：
+ * 
+ * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * 输出：7 -> 0 -> 8
+ * 原因：342 + 465 = 807
+ * 
+ * 
+ */
+/**
+ * Definition for singly-linked list. public class ListNode { int val; ListNode
+ * next; ListNode(int x) { val = x; } }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if (l1 == null || l2 == null)
+            return l1 == null ? l2 : l1;
+        int carry = 0;
+        ListNode listNode = new ListNode(-1);
+        ListNode head = listNode;//头结点指向
+        while (l1 != null || l2 != null) {
+            int sum = carry;
+            carry = 0;
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            int newVal = sum;
+            if (sum >= 10) {
+                carry = 1;// 进位
+                newVal = sum % 10;// 取余数
+            }
+            listNode.next = new ListNode(newVal);
+            listNode = listNode.next;
+        }
+        if (carry > 0) {
+            listNode.next = new ListNode(1);// carry不可能超过1，所以直接写1了
+        }
+        return head.next;
+    }
+}
---
+++ 5.最长回文子串.java
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=5 lang=java
+ *
+ * [5] 最长回文子串
+ *
+ * https://leetcode-cn.com/problems/longest-palindromic-substring/description/
+ *
+ * algorithms
+ * Medium (24.72%)
+ * Total Accepted:    43.9K
+ * Total Submissions: 176.9K
+ * Testcase Example:  '"babad"'
+ *
+ * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+ * 
+ * 示例 1：
+ * 
+ * 输入: "babad"
+ * 输出: "bab"
+ * 注意: "aba" 也是一个有效答案。
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 输入: "cbbd"
+ * 输出: "bb"
+ * 
+ * 
+ */
+class Solution {
+    public String longestPalindrome(String s) {
+        
+    }
+}
+
