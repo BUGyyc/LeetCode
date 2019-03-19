e5ffa419e17b15f134d71139f8c0775fb090dc80
BUGyyc <846353427@qq.com>
2019-03-19 12:24:59 +0800

    ...

---
+++ 23.合并k个排序链表.java
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=23 lang=java
+ *
+ * [23] 合并K个排序链表
+ *
+ * https://leetcode-cn.com/problems/merge-k-sorted-lists/description/
+ *
+ * algorithms
+ * Hard (43.82%)
+ * Total Accepted:    16.6K
+ * Total Submissions: 37.6K
+ * Testcase Example:  '[[1,4,5],[1,3,4],[2,6]]'
+ *
+ * 合并 k 个排序链表，返回合并后的排序链表。请分析和描述算法的复杂度。
+ * 
+ * 示例:
+ * 
+ * 输入:
+ * [
+ * 1->4->5,
+ * 1->3->4,
+ * 2->6
+ * ]
+ * 输出: 1->1->2->3->4->4->5->6
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        
+    }
+}
+
---
+++ 29.两数相除.java
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode.cn id=29 lang=java
+ *
+ * [29] 两数相除
+ *
+ * https://leetcode-cn.com/problems/divide-two-integers/description/
+ *
+ * algorithms
+ * Medium (17.56%)
+ * Total Accepted:    9.3K
+ * Total Submissions: 52.6K
+ * Testcase Example:  '10\n3'
+ *
+ * 给定两个整数，被除数 dividend 和除数 divisor。将两数相除，要求不使用乘法、除法和 mod 运算符。
+ * 
+ * 返回被除数 dividend 除以除数 divisor 得到的商。
+ * 
+ * 示例 1:
+ * 
+ * 输入: dividend = 10, divisor = 3
+ * 输出: 3
+ * 
+ * 示例 2:
+ * 
+ * 输入: dividend = 7, divisor = -3
+ * 输出: -2
+ * 
+ * 说明:
+ * 
+ * 
+ * 被除数和除数均为 32 位有符号整数。
+ * 除数不为 0。
+ * 假设我们的环境只能存储 32 位有符号整数，其数值范围是 [−2^31,  2^31 − 1]。本题中，如果除法结果溢出，则返回 2^31 − 1。
+ * 
+ * 
+ */
+class Solution {
+    public int divide(int dividend, int divisor) {
+        
+    }
+}
+
