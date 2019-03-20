17b58e06a26630e2c928d3286b3c379dcfc243ef
BUGyyc <846353427@qq.com>
2019-03-20 10:10:39 +0800

    备份

---
+++ 47.全排列-ii.java
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=47 lang=java
+ *
+ * [47] 全排列 II
+ *
+ * https://leetcode-cn.com/problems/permutations-ii/description/
+ *
+ * algorithms
+ * Medium (50.86%)
+ * Total Accepted:    8.4K
+ * Total Submissions: 16.4K
+ * Testcase Example:  '[1,1,2]'
+ *
+ * 给定一个可包含重复数字的序列，返回所有不重复的全排列。
+ * 
+ * 示例:
+ * 
+ * 输入: [1,1,2]
+ * 输出:
+ * [
+ * ⁠ [1,1,2],
+ * ⁠ [1,2,1],
+ * ⁠ [2,1,1]
+ * ]
+ * 
+ */
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        
+    }
+}
+
---
+++ 56.合并区间.java
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode.cn id=56 lang=java
+ *
+ * [56] 合并区间
+ *
+ * https://leetcode-cn.com/problems/merge-intervals/description/
+ *
+ * algorithms
+ * Medium (34.93%)
+ * Total Accepted:    10.9K
+ * Total Submissions: 31K
+ * Testcase Example:  '[[1,3],[2,6],[8,10],[15,18]]'
+ *
+ * 给出一个区间的集合，请合并所有重叠的区间。
+ * 
+ * 示例 1:
+ * 
+ * 输入: [[1,3],[2,6],[8,10],[15,18]]
+ * 输出: [[1,6],[8,10],[15,18]]
+ * 解释: 区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: [[1,4],[4,5]]
+ * 输出: [[1,5]]
+ * 解释: 区间 [1,4] 和 [4,5] 可被视为重叠区间。
+ * 
+ */
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+class Solution {
+    public List<Interval> merge(List<Interval> intervals) {
+        
+    }
+}
+
