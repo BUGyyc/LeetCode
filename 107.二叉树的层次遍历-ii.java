a9fc85a91f49a9731ca056c00584932a7069e8cd
BUGyyc <846353427@qq.com>
2019-03-21 10:56:13 +0800

    对称二叉树

---
+++ 107.二叉树的层次遍历-ii.java
@@ -0,0 +1,50 @@
+/*
+ * @lc app=leetcode.cn id=107 lang=java
+ *
+ * [107] 二叉树的层次遍历 II
+ *
+ * https://leetcode-cn.com/problems/binary-tree-level-order-traversal-ii/description/
+ *
+ * algorithms
+ * Easy (59.11%)
+ * Total Accepted:    11.5K
+ * Total Submissions: 19.3K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * 给定一个二叉树，返回其节点值自底向上的层次遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
+ * 
+ * 例如：
+ * 给定二叉树 [3,9,20,null,null,15,7],
+ * 
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ * 
+ * 
+ * 返回其自底向上的层次遍历为：
+ * 
+ * [
+ * ⁠ [15,7],
+ * ⁠ [9,20],
+ * ⁠ [3]
+ * ]
+ * 
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        
+    }
+}
+
---
+++ 118.杨辉三角.java
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=118 lang=java
+ *
+ * [118] 杨辉三角
+ *
+ * https://leetcode-cn.com/problems/pascals-triangle/description/
+ *
+ * algorithms
+ * Easy (59.16%)
+ * Total Accepted:    15.2K
+ * Total Submissions: 25.7K
+ * Testcase Example:  '5'
+ *
+ * 给定一个非负整数 numRows，生成杨辉三角的前 numRows 行。
+ * 
+ * 
+ * 
+ * 在杨辉三角中，每个数是它左上方和右上方的数的和。
+ * 
+ * 示例:
+ * 
+ * 输入: 5
+ * 输出:
+ * [
+ * ⁠    [1],
+ * ⁠   [1,1],
+ * ⁠  [1,2,1],
+ * ⁠ [1,3,3,1],
+ * ⁠[1,4,6,4,1]
+ * ]
+ * 
+ */
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        
+    }
+}
+
