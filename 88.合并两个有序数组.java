badba27e8d533b2db4a030cceacef6e9ebeb3b6c
BUGyyc <846353427@qq.com>
2019-03-21 10:29:49 +0800

    合并有序数组

--- 88.合并两个有序数组.java
+++ 88.合并两个有序数组.java
@@ -30,8 +30,37 @@
  * 
  */
 class Solution {
+    /**
+     * 遍历1个数组 当第二个数组可插入，则插入同时执行后移
+     */
     public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int p = m-- + n-- - 1;
+        while (m >= 0 && n >= 0) {
+            nums1[p--] = nums1[m] > nums2[n] ? nums1[m--] : nums2[n--];
+        }
         
+        while (n >= 0) {//没比较完，全部插入
+            nums1[p--] = nums2[n--];
+        }
+        // int j = 0;
+        // for (int i = 0; i < m; i++) {
+        //     if (nums2[j] < nums1[i]) {
+        //         moveArr1(nums1, i, m+j);
+        //         nums1[i] = nums2[j];
+        //         j++;
+        //     }
+        // }
+        // int x = m;
+        // while (j < n) {// nums2还没遍历完
+        //     nums1[x] = nums2[j];
+        //     x++;
+        //     j++;
+        // }
     }
-}
 
+    // private void moveArr1(int[] nums1, int index, int len) {
+    //     for (int i = len; i >= index; i--) {
+    //         nums1[i + 1] = nums1[i];
+    //     }
+    // }
+}
