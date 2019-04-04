131eb5bd4f93e946c96542f51fb7f8f861462316
BUGyyc <846353427@qq.com>
2019-04-04 14:51:42 +0800

    。

--- 14.最长公共前缀.java
+++ 14.最长公共前缀.java
@@ -34,21 +34,28 @@
  * 
  */
 class Solution {
-    //TODO:
+    // TODO:
     /**
-     * 取第一个字符做参考，和其他几个字符做比较
-     * 如果不等，就把prefix逐渐缩短，直到全部相等
+     * 取第一个字符做参考，和其他几个字符做比较 如果不等，就把prefix逐渐缩短，直到全部相等
      */
     public String longestCommonPrefix(String[] strs) {
+        return extracted(strs);
+    }
+
+    private String extracted(String[] strs) {
         if (strs.length == 0)
             return "";
         String prefix = strs[0];
         for (int i = 1; i < strs.length; i++)
-            while (strs[i].indexOf(prefix) != 0) {//从最长缩减 至 直到包含，
+            while (strs[i].indexOf(prefix) != 0) {// 从最长缩减 至 直到包含，
                 prefix = prefix.substring(0, prefix.length() - 1);
                 if (prefix.isEmpty())
                     return "";
             }
         return prefix;
+    }
+
+    private String func1(String[] strs){
+        return "";
     }
 }
---
+++ 404.左叶子之和.java
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode.cn id=404 lang=java
+ *
+ * [404] 左叶子之和
+ *
+ * https://leetcode-cn.com/problems/sum-of-left-leaves/description/
+ *
+ * algorithms
+ * Easy (48.02%)
+ * Total Accepted:    4.2K
+ * Total Submissions: 8.8K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * 计算给定二叉树的所有左叶子之和。
+ * 
+ * 示例：
+ * 
+ * 
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ * 
+ * 在这个二叉树中，有两个左叶子，分别是 9 和 15，所以返回 24
+ * 
+ * 
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        
+    }
+}
+
